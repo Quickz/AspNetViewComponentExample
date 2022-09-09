19a23b0a81709980cb42e38b3db99a80953c7db6
Quickz <alexserg@inbox.lv>
2022-09-09 21:23:03 +0300

    added the project

---
+++ AspNetViewComponentExample/Pages/Shared/Components/YoutubeVideo/YoutubeVideoViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetViewComponentExample.Pages.Shared.Components.YoutubeVideo
+{
+    public class YoutubeVideoViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(
+            string videoId, string cssClass = "")
+        {
+            var arguments = this.ViewComponentContext;
+
+            var model = new YoutubeVideoViewModel
+            {
+                VideoId = videoId,
+                Class = cssClass,
+            };
+            return View("YoutubeVideo", model);
+        }
+    }
+}
---
+++ AspNetViewComponentExample/Pages/Shared/Components/YoutubeVideo/YoutubeVideoViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace AspNetViewComponentExample.Pages.Shared.Components.YoutubeVideo
+{
+    public class YoutubeVideoViewModel
+    {
+        public string VideoId { get; set; }
+        public string Class { get; set; }
+    }
+}
